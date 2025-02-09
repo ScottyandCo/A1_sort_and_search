7bafb399ef35483f45b688549d900c55d74f85a0
Jason Scott <git@scottyand.co>
2025-02-09 16:49:20 +0800

    Added a MainGame class and moved all code into methods, completed data validation for user inputs and tidied up score output

--- Assessment1/Assessment1/Program.cs
+++ Assessment1/Assessment1/Program.cs
@@ -9,107 +9,140 @@
  *      a. The total number of values that the user must enter / are generated randomly.
  *      b. The range of numbers that are accepted for input / to be selected from randomly.
  */
-//TODO: confirm 6a and 6b are supposed to be randomly generated, or they must be able to be changed by the user?
-//TODO: add input validation, error handling and define strict value handling
 
-using System.ComponentModel.Design;
+MainGame guessingGame = new MainGame();
+int numberOfGuesses = guessingGame.DefineGameParameters();
+int[] randomNumberArray = guessingGame.GenerateRandomNumbers();
 
-int numberOfGuesses = 0;
-int developerMinRange = 0;
-int minRange = developerMinRange;
-int developerMaxRange = 1000;
-int maxRange = developerMaxRange;
-
-while (numberOfGuesses <= 0) { 
-    Console.Write("How many guesses would you like to attempt (greater than 0)? "); 
-    while (!int.TryParse(Console.ReadLine(), out numberOfGuesses)) 
-    { 
-        Console.Write("How many numbers would you like to guess (greater than 0)? "); 
-    } 
-}
-
-while (minRange <= developerMinRange)
-{
-    Console.Write($"What would you like the lowest number to be (greater than {developerMinRange})? ");
-    while (!int.TryParse(Console.ReadLine(), out minRange))
-    {
-        Console.Write($"What would you like the lowest number to be (greater than {developerMinRange})? ");
-    }
-}
-
-// Determine the maximum number available in the random number array
-// Continue to ask the user for another number if it is above the hard maximum
-while (maxRange >= developerMaxRange)
-{
-    Console.Write($"What would you like the highest number to be (less than {developerMaxRange})? ");
-    while (!int.TryParse(Console.ReadLine(), out maxRange))
-    {
-        Console.Write($"What would you like the highest number to be (less than {developerMaxRange})? ");
-    }
-}
-
-//if (maxRange - minRange > numberOfGuesses)
-    //re-run start program function
-
-int[] randomNumberArray = new int[numberOfGuesses];
-int number;
-// fill the randomNumberArray with random numbers
-for (int index = 0; index < randomNumberArray.Length; index++)
-{
-    number = new Random().Next(minRange, maxRange + 1);
-    while (randomNumberArray.Contains(number))
-        number = new Random().Next(minRange, maxRange + 1);
-    randomNumberArray[index] = number;
-}
-
-Console.WriteLine("Random Numbers {0}", string.Join(", ", randomNumberArray));
-
-int[] userGuessArray = new int[numberOfGuesses];
-for (int index = 0; index < numberOfGuesses; index++)
-{
-    int userGuess = 0;
-    while (userGuess < minRange || userGuess > maxRange)
-    {
-        Console.Write($"Enter a number (between {minRange} and {maxRange}) for Guess #{index + 1}: ");
-        userGuess = int.Parse(Console.ReadLine());
-        userGuessArray[index] = userGuess;
-    }
-}
+//TODO: Remove the below testing code
 Console.WriteLine();
-int score = 0;
-List<int> correctGuesses = new List<int>();
-for (int index = 0; index < numberOfGuesses; index++)
+Console.WriteLine("FOR TESTING PURPOSES");
+Console.WriteLine("Random Numbers {0}", string.Join(", ", randomNumberArray));
+Console.WriteLine();
+// remove above
+
+int[] userGuessArray = guessingGame.TakeUserGuess();
+(int score, List<int> correctGuesses) = guessingGame.ScoreUserGuesses(userGuessArray, numberOfGuesses, randomNumberArray);
+guessingGame.ProvideUserFeedback(score, numberOfGuesses, correctGuesses);
+
+internal class MainGame
 {
-    Console.WriteLine($"Guess #{index + 1} - You guessed '{userGuessArray.GetValue(index)}' and the correct number was '{randomNumberArray.GetValue(index)}'");
-    if (Convert.ToInt32(randomNumberArray.GetValue(index)) == Convert.ToInt32(userGuessArray.GetValue(index)))
+    // Developer hard floor and ceiling
+    private const int DeveloperMinRange = 0;
+    private const int DeveloperMaxRange = 1000;
+    // User provides the below, within the range above
+    private static int _numberOfGuesses;
+    private static int _minRange = DeveloperMinRange;
+    private static int _maxRange = DeveloperMaxRange;
+    
+    public int DefineGameParameters()
     {
-        Console.WriteLine("Congratulations! You guessed the correct number!");
-        Console.WriteLine();
-        correctGuesses.Add(userGuessArray[index]);
-        score += 1;
+        while (_numberOfGuesses <= 0 || _numberOfGuesses > DeveloperMaxRange) { 
+            Console.Write("How many guesses would you like to attempt (greater than 0)? "); 
+            while (!int.TryParse(Console.ReadLine(), out _numberOfGuesses)) 
+            { 
+                Console.Write("How many guesses would you like to attempt (greater than 0)? "); 
+            }
+        }
+
+        while (_minRange <= DeveloperMinRange)
+        {
+            Console.Write($"What would you like the lowest number to be (greater than {DeveloperMinRange})? ");
+            while (!int.TryParse(Console.ReadLine(), out _minRange))
+            {
+                Console.Write($"What would you like the lowest number to be (greater than {DeveloperMinRange})? ");
+            }
+        }
+
+        // Determine the maximum number available in the random number array
+        // Continue to ask the user for another number if it is above the developers maximum
+        while (_maxRange >= DeveloperMaxRange)
+        {
+            Console.Write($"What would you like the highest number to be (less than {DeveloperMaxRange})? ");
+            while (!int.TryParse(Console.ReadLine(), out _maxRange))
+            {
+                Console.Write($"What would you like the highest number to be (less than {DeveloperMaxRange})? ");
+            }
+        }
+
+        if (_maxRange - _minRange < _numberOfGuesses)
+        {
+            Console.WriteLine();
+            Console.WriteLine("The number of guesses exceeds the range of available numbers, enter a broader range");
+            Console.WriteLine();
+            _numberOfGuesses = 0;
+            _minRange = DeveloperMinRange;
+            _maxRange = DeveloperMaxRange;
+            DefineGameParameters();
+        } return _numberOfGuesses;
     }
-    else
+
+    public int[] GenerateRandomNumbers()
     {
-        Console.WriteLine("Unfortunately you got that one wrong");
+        int[] randomNumberArray = new int[_numberOfGuesses];
+        // fill the randomNumberArray with random numbers
+        for (int index = 0; index < randomNumberArray.Length; index++)
+        {
+            int number = new Random().Next(_minRange, _maxRange + 1);
+            while (randomNumberArray.Contains(number))
+                number = new Random().Next(_minRange, _maxRange + 1);
+            randomNumberArray[index] = number;
+        }
+        return randomNumberArray;
+    }
+
+    public int[] TakeUserGuess()
+    {
+        int[] userGuessArray = new int[_numberOfGuesses];
+        for (int index = 0; index < _numberOfGuesses; index++)
+        {
+            int userGuess = 0;
+            while (userGuess < _minRange || userGuess > _maxRange)
+            {
+                Console.Write($"Guess #{index + 1} - Enter a number (between {_minRange} and {_maxRange}): ");
+                while (!int.TryParse(Console.ReadLine(), out userGuess)) 
+                { 
+                    Console.Write($"Guess #{index + 1} - Enter a number (between {_minRange} and {_maxRange}): "); 
+                }
+                userGuessArray[index] = userGuess;
+            }
+        }
         Console.WriteLine();
+        return userGuessArray;
+    } 
+
+    public (int, List<int>) ScoreUserGuesses(int[] userGuessArray, int numberOfGuesses, int[] randomNumberArray)
+    {
+        int score = 0;
+        List<int> correctGuesses = [];
+        for (int index = 0; index < numberOfGuesses; index++)
+        {
+            Console.WriteLine($"Guess #{index + 1} - You guessed '{userGuessArray.GetValue(index)}' and the correct number was '{randomNumberArray.GetValue(index)}'");
+            if (Convert.ToInt32(randomNumberArray.GetValue(index)) == Convert.ToInt32(userGuessArray.GetValue(index)))
+            {
+                Console.WriteLine("Congratulations! You guessed the correct number!");
+                Console.WriteLine();
+                correctGuesses.Add(userGuessArray[index]);
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("Unfortunately you got that one wrong");
+                Console.WriteLine();
+            }
+        }
+        return (score, correctGuesses);
+    }
+
+    public void ProvideUserFeedback(int score, int numberOfGuesses, List<int> correctGuesses)
+    {
+        Console.WriteLine($"You correctly guessed {score} numbers out of {numberOfGuesses} attempts.");
+        if (score == 0)
+            Console.WriteLine("You didn't guess any numbers correctly...");
+        else if (score == 1)
+            Console.WriteLine("Your correct guess was number {0}", string.Join(" & ", correctGuesses));
+        else
+            Console.WriteLine("You correctly guessed numbers {0}", string.Join(" & ", correctGuesses));
     }
 }
-
-Console.WriteLine($"You correctly guessed {score} out of {numberOfGuesses}");
-Console.WriteLine("Your correct guesses were numbers {0}", string.Join(" & ", correctGuesses));
-
-/*Prog.Begin();
-
-class Prog
-{
-    private int a = 2;
-    private int b = 1;
-    private int c = 5;
-
-    public static void Begin()
-    {
-        Prog Start = new Prog();
-        Console.WriteLine($"Program Started {Start.a}");
-    }
-}*/
 
